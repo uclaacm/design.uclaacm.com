703b4f0f4e7345be341af0d4dc32258f499b2a5b
Remi Nguyen <reminguyen@Remis-MacBook-Pro.local>
2026-04-13 17:31:32 -0700

    initial commit

--- src/components/sections/AboutUs.tsx
+++ src/components/sections/AboutUs.tsx
@@ -1,15 +1,36 @@
-import ImagePlaceholder from "../common/ImagePlaceholder";
+import ImagePlaceholder from "../common/ImagePlaceholder"
 
 function AboutUs() {
     return(
-        <div style={{ padding: "0 100px" }}>
-            <p style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 600, fontSize: "2rem" }}>What is ACM Design</p>
-            <div style={{ position: "relative" }}>
-                <ImagePlaceholder src="/images/blank.png" width="100%" height="400px" />
-                <div style={{ position: "absolute", top: "20px", left: "20px", maxWidth: "50%" }}>
-                    <p style={{ fontFamily: "'Open Sans', sans-serif" }}>Lorem ipsum dolor sit amet consectetur. Placerat a quam malesuada curabitur. Pharetra nisl nisl commodo venenatis aenean leo euismod. Consequat ipsum ullamcorper in egestas id sagittis. Scelerisque dui nibh mauris bibendum in.</p>
+        <div>
+            <div style={{display: "flex", alignItems: "center"}}>
+            
+                <div>
+                    <h1 style={{ fontFamily: "Poppins, sans-serif", fontSize: "48px", fontWeight: "600" }}>
+                        About <span style={{ background: "linear-gradient(90deg, #FF7A00, #FFB800)", WebkitBackgroundClip: "text", backgroundClip: "text", WebkitTextFillColor: "transparent" }}>ACM Design</span>
+                    </h1>
+                    <h2 style={{ fontFamily: "Poppins, sans-serif", fontSize: "28px", fontWeight: "500" }}>
+                        Form Follows function
+                    </h2>
+                    <p style={{ fontFamily: "Open Sans, sans-serif", fontSize: "16px"}}>
+                        Our mission is to design, manage, and support the visual identity of ACM at
+                        UCLA. We create the club's branding, event banners, and more elements,
+                        while also doing workshops to teach UI/UX to the UCLA community.
+                    </p>
                 </div>
+
+                {/*Illustration*/}
+                <div style={{ width: "548px", height: "413px", borderRadius: "20px", backgroundColor: "#e7daca" }}>
+                    <ImagePlaceholder src="/images/blank.png" width="548px" height="413px"/>
+                </div>
+
             </div>
+
+            {/*Group photo*/}
+            <div style={{ width: "100%", height: "500px", borderRadius: "20px" }}>
+                <ImagePlaceholder src="/images/blank.png" width="100%" height="500px"/>
+            </div>
+
         </div>
     )
 }
